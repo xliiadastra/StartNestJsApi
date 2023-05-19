8c5e73284d4816c0ade77d52f1aa8ac9d5c0e1b8
xliiadastra <xlii.ad.astra@gmail.com>
2023-05-19 13:28:53 +0900

    main

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,4 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule); // AppMoudle 을 들어가보면 Class 인 걸 알 수 있다.
   await app.listen(3000);
 }
-bootstrap();
+bootstrap(); // bootstrap 은 단지 이름일 뿐이다.
